5998242ce99c93ce2ebbe86b4a342348a61e6950
Vishnu Jangid <vis18814@gmail.com>
2020-08-25 19:49:48 +0530

    Auto-generated commit

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+function opentab(evt, tabname) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabname).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+$(window).scroll(function() {
+    var body = $('body'),
+        scroll = $(window).scrollTop();
+
+    if (scroll >= 50) body.addClass('fixed');
+    else body.removeClass('fixed');
+});
+
+// offer slider
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+$('#carouselExampleIndicators').carousel('dispose');
